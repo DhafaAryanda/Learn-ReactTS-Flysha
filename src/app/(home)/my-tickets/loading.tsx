a8102fe87d15abb4ec9956f42d787d6f12382e31
dhofaaryanda4@gmail.com <dhofaaryanda4@gmail.com>
2024-10-01 22:26:00 +0700

    [FEAT] integrate CheckoutPage, Slicing SuccessCheckout, MyTicket & Detail Ticket,

--- src/app/(home)/my-tickets/loading.tsx
+++ src/app/(home)/my-tickets/loading.tsx
@@ -1,12 +1,9 @@
 import Navbar from "@/app/components/navbar";
 import React from "react";
-import FlightCard from "./components/flight-card";
-import Benefits from "./components/benefits";
-import PaymentDetail from "./components/payment-detail";
-import { getUser } from "@/lib/auth";
+import TicketCard from "./components/ticket-card";
+import LoadingTicketCard from "./components/loading-ticket-card";
 
-export default async function CheckoutPage() {
-  const { user } = await getUser();
+export default function Loading() {
   return (
     <>
       <section
@@ -16,9 +13,9 @@ export default async function CheckoutPage() {
         <div className="Header-content bg-gradient-to-r from-[#080318] to-[rgba(8,3,24,0)] h-[290px]">
           <Navbar />
           <div className="title container max-w-[1130px] mx-auto flex flex-col gap-1 pt-[50px] pb-[68px]">
-            <h1 className="font-bold text-[32px] leading-[48px]">Checkout</h1>
+            <h1 className="font-bold text-[32px] leading-[48px]">My Tickets</h1>
             <p className="font-medium text-lg leading-[27px]">
-              Enjoy new experience of flight
+              183,042 flights avaiable
             </p>
           </div>
           <div className="w-full h-[15px] bg-gradient-to-t from-[#080318] to-[rgba(8,3,24,0)] absolute bottom-0"></div>
@@ -26,14 +23,16 @@ export default async function CheckoutPage() {
       </section>
       <section
         id="Content"
-        className="container max-w-[1130px] mx-auto -mt-[33px] z-10 relative"
+        className="container max-w-[1130px] mx-auto flex justify-end -mt-[60px] pb-[100px] z-10 relative"
       >
-        <div className="checkout-container flex flex-col lg:flex-row gap-[70px]">
-          <FlightCard user={user} />
-          <div className="flex flex-col mt-[63px] gap-[30px]">
-            <Benefits />
-            <PaymentDetail user={user} />
-          </div>
+        <div className="ticket-container flex flex-col w-[900px] gap-6">
+          <LoadingTicketCard />
+          <LoadingTicketCard />
+          <LoadingTicketCard />
+          <LoadingTicketCard />
+          <p className="text-center text-sm text-[#A0A0AC] h-fit">
+            You’ve reached the end of results.
+          </p>
         </div>
       </section>
     </>
---
+++ src/hooks/useTransaction.tsx
@@ -0,0 +1,85 @@
+import { User } from "lucia";
+import useCheckoutData from "./useCheckoutData";
+import { useMemo, useState } from "react";
+import { SEAT_VALUES, type SeatValuesType } from "@/lib/utils";
+import { useMutation } from "@tanstack/react-query";
+import axios from "axios";
+import { useRouter } from "next/navigation";
+
+type Props = {
+  user: User | null;
+};
+
+const useTransaction = ({ user }: Props) => {
+  const { data } = useCheckoutData();
+
+  const [isLoading, setIsLoading] = useState<boolean>(false);
+
+  const router = useRouter();
+
+  const selectedSeat = useMemo(() => {
+    return SEAT_VALUES[(data?.seat as SeatValuesType) ?? "ECONOMY"];
+  }, [data?.seat]);
+
+  const transactionMutate = useMutation({
+    mutationFn: (data: any) =>
+      axios.post("/api/transactions/create", data).then((res) => res.data),
+  });
+  const payTransaction = async () => {
+    if (!data && user) {
+      return null;
+    }
+
+    const totalPrice = Number(
+      data?.flightDetail?.price ?? 0 + selectedSeat.additionalPrice
+    );
+
+    const bodyData = {
+      bookingDate: new Date(),
+      customerId: user?.id,
+      flightId: data?.flightDetail?.id,
+      price: totalPrice,
+      seatId: data?.seatDetail?.id,
+      departureCityCode: data?.flightDetail?.departureCityCode,
+      destinationCityCode: data?.flightDetail?.departureCityCode,
+    };
+
+    try {
+      setIsLoading(true);
+      const transaction = await transactionMutate.mutateAsync(bodyData);
+
+      // handle midtrans
+      window.snap.pay(transaction.midtrans.token, {
+        onSuccess: (result: any) => {
+          alert("payment success!");
+          console.log(result);
+          router.push("/success-checkout");
+        },
+        onPending: (result: any) => {
+          alert("payment success!");
+          router.push("/success-checkout");
+          console.log(result);
+        },
+        onError: (result: any) => {
+          alert("Transaksi gagal silahkan coba lagi");
+          console.log(result);
+        },
+        onClose: (result: any) => {
+          alert("Transaksi gagal silahkan coba lagi");
+          console.log(result);
+        },
+      });
+
+      setIsLoading(false);
+    } catch (error) {
+      setIsLoading(false);
+      console.log(error);
+    }
+  };
+  return {
+    payTransaction,
+    isLoading,
+  };
+};
+
+export default useTransaction;
